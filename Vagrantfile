d319192cec8003fc6c18a98d3745b36a0f3c55b6
Austin Heiman <atheimanksu@gmail.com>
2014-12-10 23:46:23 -0600

    testing vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "hashicorp/precise64"
 
-  config.vm.network "forwarded_port", guest: 80, host: 8000
+  config.vm.network "forwarded_port", guest: 8000, host: 8000
+  config.vm.network "forwarded_port", guest: 80, host: 8080
 
   config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,71 +1,81 @@
 #!/usr/bin/env bash
 
+
+
+# Shell script to deploy mis-club-site to a vm provisioned by vagrant.
+
+
+
 # Global vars
-PROJ_TEMPLATE_PATH=/tmp/project_name
-DJANGO_PROJECT_DIR=/opt/django_project
-VIRTUAL_ENV=django_env
-DJANGO_PROJECT=my_site
-WSGI_RELATIVE_PATH=my_site.py
-APACHE_VHOST=django_host
+PROJ_NAME=mis-club-site
+PROJ_DIR=/opt/$PROJ_NAME
+TEMP_DIR=/tmp/$PROJ_NAME
+
+VIRTUALENV=$PROJ_DIR/env
+REQUIREMENTS_FILE=$PROJ_DIR/conf/requirements.txt
+
+GIT_CLONE_URL=git@github.com:atheiman/mis-club-site.git
+GIT_BRANCH=tag/1.0.1
+
+WSGI_RELATIVE_PATH=conf/wsgi.py
+APACHE_VHOST=misclub
 APACHE_SITES_AVAILABLE_DIR=/etc/apache2/sites-available
 
 
 
 # clean up from previous provisions
-rm -rf $PROJ_TEMPLATE_PATH $DJANGO_PROJECT_DIR
-mkdir $PROJ_TEMPLATE_PATH $DJANGO_PROJECT_DIR
+rm -rf $PROJ_DIR $TEMP_DIR /etc/apache2/sites-enabled/*
+mkdir $PROJ_DIR $TEMP_DIR
 
 
 
 # Install OS packages
 apt-get update
-apt-get install -y apache2 python-pip libapache2-mod-wsgi curl
+apt-get install --yes python-pip apache2 libapache2-mod-wsgi git
 
 
 
-# Setup a virtualenv
+# # Get the source
+git clone --branch=$GIT_BRANCH $GIT_CLONE_URL $PROJ_DIR
+
+
+
+# # Setup a virtualenv
 pip install virtualenv
-virtualenv $DJANGO_PROJECT_DIR/$VIRTUAL_ENV
-$DJANGO_PROJECT_DIR/$VIRTUAL_ENV/bin/pip install django
+virtualenv $VIRTUALENV
+$VIRTUALENV/bin/pip install --requirement=$REQUIREMENTS_FILE
 
 
 
-# Create Django project from template
-curl https://gist.githubusercontent.com/atheiman/e7b8bec6f67689d61c1c/raw/project_name.py > $PROJ_TEMPLATE_PATH/project_name.py
-cd $DJANGO_PROJECT_DIR
-$DJANGO_PROJECT_DIR/$VIRTUAL_ENV/bin/django-admin.py startproject $DJANGO_PROJECT --template=$PROJ_TEMPLATE_PATH
+$VIRTUALENV/bin/python $PROJ_DIR/manage.py runserver 0.0.0.0:8000
 
 
 
-# Run the Django dev server
-# $DJANGO_PROJECT_DIR/$VIRTUAL_ENV/bin/python $DJANGO_PROJECT/$WSGI_RELATIVE_PATH runserver 0.0.0.0:8080
+# # Apache HTTP Server config
+# rm -rf /etc/apache2/sites-enabled/*
 
 
+# # Create a Django virtual host
+# echo "" > $APACHE_SITES_AVAILABLE_DIR/$APACHE_VHOST
 
-# Apache HTTP Server config
-a2dissite default
+# cat <<EOT >> $APACHE_SITES_AVAILABLE_DIR/$APACHE_VHOST
+# <VirtualHost *:80>
 
-# Create a Django virtual host
-touch $APACHE_SITES_AVAILABLE_DIR/$APACHE_VHOST
+#     WSGIDaemonProcess $DJANGO_PROJECT python-path=$DJANGO_PROJECT_DIR/$DJANGO_PROJECT:$DJANGO_PROJECT_DIR/$VIRTUAL_ENV/lib/python2.7/site-packages
+#     WSGIProcessGroup $DJANGO_PROJECT
+#     WSGIScriptAlias / $DJANGO_PROJECT_DIR/$DJANGO_PROJECT/$WSGI_RELATIVE_PATH
 
-cat <<EOT >> $APACHE_SITES_AVAILABLE_DIR/$APACHE_VHOST
-<VirtualHost *:80>
+#     ServerAdmin atheimanksu@gmail.com
 
-    WSGIDaemonProcess $DJANGO_PROJECT python-path=$DJANGO_PROJECT_DIR/$DJANGO_PROJECT:$DJANGO_PROJECT_DIR/$VIRTUAL_ENV/lib/python2.7/site-packages
-    WSGIProcessGroup $DJANGO_PROJECT
-    WSGIScriptAlias / $DJANGO_PROJECT_DIR/$DJANGO_PROJECT/$WSGI_RELATIVE_PATH
+#     ErrorLog ${APACHE_LOG_DIR}/error.log
 
-    ServerAdmin atheimanksu@gmail.com
+#     LogLevel warn
 
-    ErrorLog ${APACHE_LOG_DIR}/error.log
+#     CustomLog ${APACHE_LOG_DIR}/access.log combined
 
-    LogLevel warn
+# </VirtualHost>
+# EOT
 
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-</VirtualHost>
-EOT
-
-# Enable your Django virtual host
-a2ensite $APACHE_VHOST
-service apache2 restart
+# # Enable your Django virtual host
+# a2ensite $APACHE_VHOST
+# service apache2 restart
