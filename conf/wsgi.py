a7bf39f83456aae9abe0fa64922fcf154de3f30e
Austin Heiman <atheimanksu@gmail.com>
2014-12-14 20:40:11 -0600

    aborting apache mod_wsgi and lighttpd for nginx

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_site project.
+WSGI config for mis-club-site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
