9c47028df301eeed8984a14ca7892d272ff32461
Austin Heiman <atheimanksu@gmail.com>
2014-12-15 08:40:19 -0600

    changed static url for prod

--- settings/prod.py
+++ settings/prod.py
@@ -13,4 +13,4 @@ ALLOWED_HOSTS = ['austinheiman.com', '*.austinheiman.com']
 
 DATABASES = PROD_DB
 
-STATIC_URL = 'http://austinheiman.com:/static/'
+STATIC_URL = 'http://austinheiman.com:/staticfiles/'
